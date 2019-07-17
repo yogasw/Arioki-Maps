5d15ae5526b84e4ecca26057922db9e3a8c86551
Yoga Setiawan <mryoga1995@gmail.com>
2019-07-17 13:32:44 +0700

    add chat

--- App/Components/Chat.js
+++ App/Components/Chat.js
@@ -1,69 +1,96 @@
 import React, {Component} from 'react';
-import {Alert, FlatList, ScrollView, Text, TextInput, TouchableHighlight, TouchableOpacity, View} from 'react-native';
+import {
+    Alert, AsyncStorage,
+    FlatList,
+    Text,
+    TextInput,
+    TouchableHighlight,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 import firebase from 'firebase';
 import 'firebase/auth';
-import {createDataUser} from '../Helper/Database';
+import SvgUri from 'react-native-svg-uri';
 
 export default class Register extends Component {
     constructor() {
         super();
         this.state = {
-            email: '',
-            chat: [1, 2, 3, 4, 1, 2, 3, 4],
+            chat: [1],
+            onEdit:false,
+            uid:'',
+            data:{},
+            text:''
         };
+        this.getUid();
     }
 
+    getUid = async () => {
+        await AsyncStorage.getItem('uid')
+            .then(data => {
+                this.setState({uid: data});
+                this.getData(data);
+            });
+    };
+    sendText (){
+        console.log(this.state)
+        let db = firebase.database();
+        const userReft = db.ref(`Chat/${this.state.uid}`)
+
+    }
+    getData(uid){
+
+    }
     hideModal() {
         this.props.hide();
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            noPhone: '',
-        };
-    }
-
-    signUp() {
-        const {name, email, password, noPhone} = this.state;
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then((data) => {
-                createDataUser(data.user.uid, name, email, noPhone);
-                Alert.alert('Alert', 'Registration Success');
-            }).catch(e => {
-            Alert.alert('Alert', e.message);
-        });
-
     }
 
     render() {
+        let data = this.props.item;
+        console.log(this.state);
         return (
             <TouchableOpacity onPress={() => this.hideModal()} activeOpacity={1} style={styles.container}>
                 <TouchableOpacity activeOpacity={1}>
                     <View style={styles.form}>
-                        <Text style={styles.textHeader}>Name</Text>
-                        <FlatList style={styles.chat} showsVerticalScrollIndicator={false} data={this.state.chat} renderItem={({item, index}) =>
-                            <TouchableHighlight>
-                                <View>
-                                <View>
-                                    <View style={styles.chatSend}>
-                                        <Text style={styles.chatTextSend}>Lorem Ipsum is simply dummy text of the
-                                            printing and
-                                            typesetting industry. Lorem Ipsum has been the industry's standard dummy
-                                            text ever
-                                            since the 1500s, when an unknown printer t</Text>
-                                    </View>
-                                    <View style={styles.margin}/>
-                                    <View style={styles.chatRev}>
-                                        <Text style={styles.chatTextRev}>Lorem Ipsum is simply dummy text of the printing and
-                                            typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
-                                            since the 1500s, when an unknown printer t</Text>
-                                    </View>
-                                </View>
-                                    <View style={styles.margin}/>
-                                </View>
-                            </TouchableHighlight>
-                        }/>
-                        <TextInput multiline={true} placeholder={"input pesan anda"}/>
+                        <Text style={styles.textHeader}>{data.name}</Text>
+                        <FlatList style={styles.chat} showsVerticalScrollIndicator={false} data={this.state.chat}
+                                  renderItem={({item, index}) =>
+                                      <TouchableHighlight>
+                                          <View>
+                                              <View>
+                                                  <View style={styles.chatSend}>
+                                                      <Text style={styles.chatTextSend}>Lorem Ipsum is simply dummy text
+                                                          of the</Text>
+                                                  </View>
+                                                  <View style={styles.margin}/>
+                                                  <View style={styles.chatRev}>
+                                                      <Text style={styles.chatTextRev}>Lorem Ipsum is simply dummy text
+                                                          of the </Text>
+                                                  </View>
+                                              </View>
+                                              <View style={styles.margin}/>
+                                          </View>
+                                      </TouchableHighlight>
+                                  }/>
+                        <View style={{flexDirection: 'row', alignItems:'center'}}>
+                            <TextInput
+                                style={styles.inputChat}
+                                multiline={true}
+                                placeholder={'input pesan anda'}
+                                onChangeText={(text => this.setState({text}))}
+                                onFocus={()=>this.setState({onEdit:true})}
+                                onBlur={()=>this.setState({onEdit:false})}
+                            />
+                           <TouchableOpacity onPress={()=>this.sendText()} style={styles.iconButtom}>
+                            <SvgUri
+                                width="30"
+                                height="30"
+                                source={
+                                    (this.state.onEdit)?require('../Assets/baseline-send-24px.svg'):
+                                        require('../Assets/baseline-mic-24px.svg')
+                                }/>
+                           </TouchableOpacity>
+                        </View>
                     </View>
                 </TouchableOpacity>
             </TouchableOpacity>
@@ -82,9 +109,10 @@ const styles = {
         backgroundColor: '#008dff',
         borderRadius: 20,
         padding: 20,
+        paddingBottom: 10,
     },
     textHeader: {
-        color:"#fff",
+        color: '#fff',
         textAlign: 'center',
         fontWeight: 'bold',
         fontSize: 20,
@@ -121,4 +149,22 @@ const styles = {
     margin: {
         marginTop: 20,
     },
+    inputChat: {
+        width:'85%',
+        marginTop: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        paddingRight: 5,
+        paddingLeft: 10,
+        marginRight: 5,
+    },
+    iconChat: {
+
+    },
+
+    iconButtom:{
+        padding:10,
+        borderRadius:50,
+        backgroundColor:"#4977ee"
+    }
 };
